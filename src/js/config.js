05c6dd98b7f4484c3a630b7c7ee9bea6e88a44a0
mohamed saber <mohamed.saber@edu.uiz.ac.ma>
2023-01-14 02:54:48 +0100

    Done ✔

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = "a7e0c817-9849-4e82-b79f-193267d55110";
