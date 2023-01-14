d5581670da5efd48017faa9d4b213074befe9499
mohamed saber <mohamed.saber@edu.uiz.ac.ma>
2023-01-14 02:39:40 +0100

    fix some deployements issues

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,4 +1,4 @@
-import icons from 'url:../../img/icons.svg'; // Parcel 2
+import icons from "url:../../img/icons.svg"; // Parcel 2
 
 export default class View {
   _data;
@@ -9,7 +9,7 @@ export default class View {
    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Jonas Schmedtmann
+   * @author Saber Mohamed
    * @todo Finish implementation
    */
   render(data, render = true) {
@@ -22,7 +22,7 @@ export default class View {
     if (!render) return markup;
 
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
   update(data) {
@@ -30,8 +30,8 @@ export default class View {
     const newMarkup = this._generateMarkup();
 
     const newDOM = document.createRange().createContextualFragment(newMarkup);
-    const newElements = Array.from(newDOM.querySelectorAll('*'));
-    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+    const newElements = Array.from(newDOM.querySelectorAll("*"));
+    const curElements = Array.from(this._parentElement.querySelectorAll("*"));
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
@@ -40,7 +40,7 @@ export default class View {
       // Updates changed TEXT
       if (
         !newEl.isEqualNode(curEl) &&
-        newEl.firstChild?.nodeValue.trim() !== ''
+        newEl.firstChild?.nodeValue.trim() !== ""
       ) {
         // console.log('💥', newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
@@ -48,14 +48,14 @@ export default class View {
 
       // Updates changed ATTRIBUES
       if (!newEl.isEqualNode(curEl))
-        Array.from(newEl.attributes).forEach(attr =>
+        Array.from(newEl.attributes).forEach((attr) =>
           curEl.setAttribute(attr.name, attr.value)
         );
     });
   }
 
   _clear() {
-    this._parentElement.innerHTML = '';
+    this._parentElement.innerHTML = "";
   }
 
   renderSpinner() {
@@ -67,7 +67,7 @@ export default class View {
       </div>
     `;
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
   renderError(message = this._errorMessage) {
@@ -82,7 +82,7 @@ export default class View {
       </div>
     `;
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
   renderMessage(message = this._message) {
@@ -97,6 +97,6 @@ export default class View {
       </div>
     `;
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 }
